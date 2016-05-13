ca261508694031e1a48930c5861f644899766f77
ccasal <casal.christian@gmail.com>
2016-05-13 02:23:37 -0400

    addin jaws_db

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: '',
-  user: '',
-  password: '',
+  host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'cgdorkkoctwknttz',
+  password: 'k0g0f5707smo5bd1',
   database: 'burgers_db'
 });
 
