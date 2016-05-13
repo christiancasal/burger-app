4a621f2236dfaf04541bd3e5d0eb10a955c9f82e
ccasal <casal.christian@gmail.com>
2016-05-13 02:41:40 -0400

    addin jaws_db

--- config/connection.js
+++ config/connection.js
@@ -14,15 +14,28 @@
 // });
 
 var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection;
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+}
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+//
+//   console.log('The solution is: ', rows[0].solution);
+// });
+//
+// connection.end();
 
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
 module.exports = connection;
